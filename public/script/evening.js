97b40df8747ce5ecdf336a134d588fd0272b90b4
Kanlayaphat <mybile.e70e@gmail.com>
2022-12-17 21:02:46 +0700

    new commit

--- public/script/evening.js
+++ public/script/evening.js
@@ -55,9 +55,9 @@ function showChat() {
 function change_bg(clicked_id)
 {
     if (clicked_id == 1) {
-        document.getElementById("bg_img").src="https://i.pinimg.com/originals/ef/21/20/ef212097b6111cd26f33b5373dbaf544.gif";
+        document.getElementById("bg_img").src="https://c0.wallpaperflare.com/preview/312/791/674/greece-sunset-west-topio.jpg";
       } else if (clicked_id == 2) {
-        document.getElementById("bg_img").src="https://i.pinimg.com/originals/4e/80/38/4e8038228e029c3ddd9c7b90b293af9d.gif";
+        document.getElementById("bg_img").src="https://images.pexels.com/photos/33545/sunrise-phu-quoc-island-ocean.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
       } else if (clicked_id == 3) {
         document.getElementById("bg_img").src="https://i.pinimg.com/originals/85/bf/2c/85bf2cd735d075bb9ece4be5996605b1.gif";
       }
--- public/script/minimalbrown.js
+++ public/script/minimalbrown.js
@@ -55,18 +55,10 @@ function showChat() {
 function change_bg(clicked_id)
 {
     if (clicked_id == 1) {
-        document.getElementById("bg_img").src="https://w0.peakpx.com/wallpaper/321/117/HD-wallpaper-afternoon-sea-colorful-glow-ocean-pier-colors-beautiful-sky-clouds-lake-mirrored-sea-afternoon-water-nature-morning-reflection.jpg";
+        document.getElementById("bg_img").src="image/minimalbrownb.png";
       } else if (clicked_id == 2) {
-        document.getElementById("bg_img").src="https://i.pinimg.com/originals/4e/80/38/4e8038228e029c3ddd9c7b90b293af9d.gif";
-      } else if (clicked_id == 3) {
-        document.getElementById("bg_img").src="https://i.pinimg.com/originals/85/bf/2c/85bf2cd735d075bb9ece4be5996605b1.gif";
-      }
-      else if (clicked_id == 4) {
-        document.getElementById("bg_img").src="https://i.pinimg.com/originals/52/86/71/528671b25753cd9e4f5e48c1586ae88b.gif";
-      } else {
-		document.getElementById("bg_img").src="https://i.pinimg.com/originals/be/1a/33/be1a338704efc57093b6dec8e196439b.gif";
-	  }
-    //document.getElementById("bg_img").src="image/afternoon.jpg";
+        document.getElementById("bg_img").src="image/minimalGirl.png";
+      } 
 }
 /*
 const image_input = document.querySelector("#file");
